1435d3a47cf5c9b64307995684514b6608b66677
AlxMen <alejandrocs402@gmail.com>
2023-03-15 18:32:12 +0000

    inicio con tailwing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
+
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="container mx-auto mt-20">
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+
+  return(
+    <div>
+      <h1>Hello World desde header</h1>
+      <p>holamundi</p>
+    </div>
+  )
+}
+
+export default Header
